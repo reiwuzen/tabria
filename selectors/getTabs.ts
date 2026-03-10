e98ecf7ae715330a6f149443e59dfa6458f630a1
Rei WuZen <reiwuzen@gmail.com>
2026-03-10 23:09:20 +0530

    feat(getTabs): implement getTabs selectors
    Returns a safe copy of all tabs in the workspace.

---
+++ selectors/getTabs.ts
@@ -0,0 +1,10 @@
+import type { Tab } from "../types/tab";
+import type { WorkspaceState } from "../types/workspace";
+import { cloneTab } from "./clone";
+
+/**
+ * Returns a safe copy of all tabs in the workspace.
+ */
+export const getTabs = (state: WorkspaceState): Tab[] => {
+  return state.tabs.map((tab) => cloneTab(tab));
+};
